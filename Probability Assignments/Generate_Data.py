7ed3a4acf4e45112c29a59f02c643e26d9945a82
atul219 <33904670+atul219@users.noreply.github.com>
2018-09-05 20:39:37 +0530

    Add files via upload

---
+++ Probability Assignments/Generate_Data.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 20:16:53 2018
+
+@author: Atul
+"""
+
+import random 
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+Mean = 10.6
+StdDev = 3.5
+
+Observations = []
+X_axis = np.linspace(-3.4,24.6,20)
+BarWidth = (24.6 + 3.4)/20
+Frequency = [0] * X_axis
+
+for i in range(0,1000):
+    
+    StdDevMultiply = random.uniform(1,4)
+    
+    Observations.append(Mean + (StdDevMultiply*StdDev))
+    
+    Observations.append(Mean - (StdDevMultiply*StdDev))
+    
+    Observations.sort()
+    
+print(Observations)
+
+for Ob in Observations:
+    
+    Frequency[math.floor((Ob -(-3.4))/BarWidth)] += 1
+    
+print(Frequency)
+
+plt.bar(X_axis,Frequency,5.6)
